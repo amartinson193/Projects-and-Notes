5709e7e5ab7abee909b85f8d72ac5022ac9d6292
Andreas Martinson <mar12041@byui.edu>
2021-01-12 16:44:39 -0700

    Adding notes

---
+++ src/Practice_Notes/start.py
@@ -0,0 +1,15 @@
+def validBraces(string):
+    sets = {'{':'}','[':']','(':')'
+           , '}':None,']':None,')':None}
+    if type(string) == str:
+        string = [char for char in string]
+    count = 0
+    while count + 1 < len(string):
+        if string[count + 1] == sets[string[count]]:
+            del string[count: count + 2]
+            validBraces(string)
+        count += 1
+    return True if len(string) == 0 else False       
+
+validBraces('{}[{}{}[[[]]]]]')
+print('hi')
