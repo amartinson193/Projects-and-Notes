a67fc9e889da9e43544b7a0b421c574532095ddb
Andreas Martinson <mar12041@byui.edu>
2021-01-13 08:18:51 -0700

    Updated notes - codewars

--- src/Practice_Notes/January 2021.py
+++ src/Practice_Notes/January 2021.py
@@ -29,10 +29,29 @@
 
 # Codewars challenge - Multiples of 3 or 5 - 6 kyu --------------------------------
 
-def solution(number):
-    total = 0
-    for i in range(number):
-        if (i % 3 == 0 or i % 5 == 0) and i >= 0:
-            total += i
-    return total
-print(solution(100))
+# def solution(number):
+#     total = 0
+#     for i in range(number):
+#         if (i % 3 == 0 or i % 5 == 0) and i >= 0:
+#             total += i
+#     return total
+# print(solution(100))
+
+# 1/13/21 --------------------------------------------------------------------
+
+# Codewars challenge - Find the odd int - 6kyu --------------------------------
+
+# import pandas as pd
+
+# def find_it(seq):
+#     x = (pd.Series(seq).value_counts() % 2 > 0)
+#     return int(x[x].index.values)
+
+# print(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]))
+
+# Codewars challenge - Find the odd int - 6kyu --------------------------------
+
+def song_decoder(song):
+    return song.replace('WUB',' ').split(' ')
+
+print(song_decoder('AAWUBWUBBWUBC'))
